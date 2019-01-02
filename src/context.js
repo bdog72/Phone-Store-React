b1824133839952635a62ca2c38cb613fd41fce28
Brian Bycynski <high5bri@hotmail.com>
2019-01-02 05:24:24 -0500

    finished lecture 106

--- src/context.js
+++ src/context.js
@@ -10,7 +10,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -72,6 +75,22 @@ class ProductProvider extends Component {
     });
   };
 
+  increment = id => {
+    console.log('This is increment method');
+  };
+
+  decrement = id => {
+    console.log('This is decrement method');
+  };
+
+  removeItem = id => {
+    console.log('This is item removed');
+  };
+
+  clearCart = () => {
+    console.log('Cart was cleared');
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -80,7 +99,11 @@ class ProductProvider extends Component {
           handelDetail: this.handelDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
